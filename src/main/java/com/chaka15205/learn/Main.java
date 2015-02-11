e06584ac9e6a76b09462c0355457884e80183104
Chaka15205 <minecraftandmore86@gmail.com>
2015-02-11 13:11:52 -0800

    Episode 6 - Getting User Input

--- src/main/java/com/chaka15205/learn/Main.java
+++ src/main/java/com/chaka15205/learn/Main.java
@@ -1,20 +1,10 @@
 package com.chaka15205.learn;
 
-/**
- * Created by Chaka on 2/11/2015.
- * Copyright 2015
- * JavaTutorials
- * com.chaka15205.learn
- */
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]) {
-
-        double tuna;
-        tuna = 5.28;
-
-        System.out.print("I want ");
-        System.out.print(tuna);
-        System.out.println(" movies");
-        System.out.print("apples");
+        Scanner s = new Scanner(System.in);
+        System.out.println(s.nextLine());
     }
 }
