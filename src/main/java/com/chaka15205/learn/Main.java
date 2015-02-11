e4eb16c96ed510647ef946a19f5f94a45978d1f0
Chaka15205 <minecraftandmore86@gmail.com>
2015-02-11 13:49:15 -0800

    Episode 8 - Math Operators

--- src/main/java/com/chaka15205/learn/Main.java
+++ src/main/java/com/chaka15205/learn/Main.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
-        double n1, n2, a;
+        int g, b, p;
+        g = 7;
+        b = 3;
+        p = g % b;
 
-        System.out.println("Enter First Number");
-        n1 = s.nextDouble();
-        System.out.println("Enter Second Number");
-        n2 = s.nextDouble();
-        a = n1 + n2;
-        System.out.println("Your Answer Is: " + a);
+        //% = remainder finder
+
+        System.out.println(p);
     }
 }
