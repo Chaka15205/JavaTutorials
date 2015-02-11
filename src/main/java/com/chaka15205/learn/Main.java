8f5a40c564ce25438b2f07a053c8dba575cebf8e
Chaka15205 <minecraftandmore86@gmail.com>
2015-02-11 13:05:34 -0800

    Episode 5 - Variables

--- src/main/java/com/chaka15205/learn/Main.java
+++ src/main/java/com/chaka15205/learn/Main.java
@@ -12,6 +12,9 @@ public class Main {
         double tuna;
         tuna = 5.28;
 
+        System.out.print("I want ");
         System.out.print(tuna);
+        System.out.println(" movies");
+        System.out.print("apples");
     }
 }
