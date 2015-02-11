0bb0f6218fad813cab943f063ec53c986f39d6eb
Chaka15205 <minecraftandmore86@gmail.com>
2015-02-11 13:43:20 -0800

    Episode 7 - Building A Basic Calculator

--- src/main/java/com/chaka15205/learn/Main.java
+++ src/main/java/com/chaka15205/learn/Main.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
-        System.out.println(s.nextLine());
+        double n1, n2, a;
+
+        System.out.println("Enter First Number");
+        n1 = s.nextDouble();
+        System.out.println("Enter Second Number");
+        n2 = s.nextDouble();
+        a = n1 + n2;
+        System.out.println("Your Answer Is: " + a);
     }
 }
