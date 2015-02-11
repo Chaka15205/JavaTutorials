797e0f2d1bd8d2a099e298be57767ae8a91feb13
Chaka15205 <minecraftandmore86@gmail.com>
2015-02-11 12:39:44 -0800

    Episode 4 - Hello World

---
+++ src/main/java/com/chaka15205/learn/Main.java
@@ -0,0 +1,15 @@
+package com.chaka15205.learn;
+
+/**
+ * Created by Chaka on 2/11/2015.
+ * Copyright 2015
+ * JavaTutorials
+ * com.chaka15205.learn
+ */
+public class Main {
+
+    public static void main(String args[]) {
+
+        System.out.println("Hello, World!");
+    }
+}
