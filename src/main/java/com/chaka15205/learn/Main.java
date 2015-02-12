c828be8919c3b9e69f0761da2da12747e821a7ce
Chaka15205 <minecraftandmore86@gmail.com>
2015-02-12 13:35:53 -0800

    Episode 19 - else if Statement

--- src/main/java/com/chaka15205/learn/Main.java
+++ src/main/java/com/chaka15205/learn/Main.java
@@ -2,17 +2,14 @@ package com.chaka15205.learn;
 
 public class Main {
     public static void main(String args[]) {
-        int age = 60;
+        int age = 45;
 
-        if (age < 50) {
-            System.out.println("Your Are Young.");
-        } else {
-            System.out.println("Your Are Old.");
-            if (age > 75) {
-                System.out.println("You Are Really Old.");
-            } else {
-                System.out.println("Don't worry you aren't really that old.");
-            }
+        if (age >= 60) {
+            System.out.println("Your Are a Senior.");
+        } else if (age >= 50){
+            System.out.println("Your Are In your 50's");
+        } else if (age >= 40) {
+            System.out.println("Your Are in your 40's.");
         }
     }
 }
