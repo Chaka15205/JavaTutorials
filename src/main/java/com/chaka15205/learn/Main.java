cfbbdaa7a92175e002a03a1bb6773fdf4294b87d
Chaka15205 <minecraftandmore86@gmail.com>
2015-02-12 12:42:31 -0800

    Episode 13 - While Loop

--- src/main/java/com/chaka15205/learn/Main.java
+++ src/main/java/com/chaka15205/learn/Main.java
@@ -4,22 +4,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-        int age;
-        age = 7;
+        int c = 0;
 
-        switch (age) {
-            case 1:
-                System.out.println("You Can Crawl.");
-                break;
-            case 2:
-                System.out.println("You Can Talk.");
-                break;
-            case 3:
-                System.out.println("You Can get in trouble.");
-                break;
-            default:
-                System.out.println("ERROR: Invalid Age");
-                break;
+        while (c < 10) {
+            System.out.println(c);
+            c++;
         }
     }
 }
