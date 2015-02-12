3451a33f4fd2eed63d72ded273a59c7a3265fd11
Chaka15205 <minecraftandmore86@gmail.com>
2015-02-12 14:24:38 -0800

    Episode 23 - Compound Interest Program

--- src/main/java/com/chaka15205/learn/Main.java
+++ src/main/java/com/chaka15205/learn/Main.java
@@ -2,11 +2,15 @@ package com.chaka15205.learn;
 
 public class Main {
     public static void main(String args[]) {
-        for (int c = 6; c <= 21; c += 3) {
-            System.out.println(c);
+        //A=P(1+R)^N
+
+        double a;
+        double p = 8;
+        double r = .01;
+
+        for (int d = 1; d <= 30; d++) {
+            a = p * Math.pow(1 + r, d);
+            System.out.println(d + "\t" + a);
         }
-        /**
-         * for (Where You Want To Start; Where You Want To End; Increment By i.e 2's 4's) {}
-         */
     }
 }
