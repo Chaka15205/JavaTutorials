4f034e9af41d680c0b8faf9cd0d48e9c4356e283
Chaka15205 <minecraftandmore86@gmail.com>
2015-02-12 14:34:27 -0800

    Episode 24 - do while Loops

--- src/main/java/com/chaka15205/learn/Main.java
+++ src/main/java/com/chaka15205/learn/Main.java
@@ -2,15 +2,12 @@ package com.chaka15205.learn;
 
 public class Main {
     public static void main(String args[]) {
-        //A=P(1+R)^N
+        int c = 15;
 
-        double a;
-        double p = 8;
-        double r = .01;
-
-        for (int d = 1; d <= 30; d++) {
-            a = p * Math.pow(1 + r, d);
-            System.out.println(d + "\t" + a);
-        }
+        // Do is for at least one run / While is only when true
+        do {
+            System.out.println(c);
+            c++;
+        } while (c <= 10);
     }
 }
