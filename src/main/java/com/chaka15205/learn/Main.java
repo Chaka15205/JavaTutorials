793bec56dd9ac704e945f81da434c81b58f5606a
Chaka15205 <minecraftandmore86@gmail.com>
2015-02-12 14:06:53 -0800

    Episode 21 - Simple Average Program

--- src/main/java/com/chaka15205/learn/Main.java
+++ src/main/java/com/chaka15205/learn/Main.java
@@ -1,9 +1,21 @@
 package com.chaka15205.learn;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]) {
-        int age = 19;
+        Scanner scan = new Scanner(System.in);
+        int grade, avg, total, count;
+        total = 0;
+        count = 0;
 
-        System.out.println(age > 50 ? "You Are Old" : "You Are Young");
+        while (count < 10) {
+            grade = scan.nextInt();
+            total = total + grade;
+            count++;
+        }
+
+        avg = total/10;
+        System.out.println("Your average is: " + avg);
     }
 }
