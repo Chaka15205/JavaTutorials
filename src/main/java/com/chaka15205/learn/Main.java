d9d8e5edc571c473d780f80d240cbc37fb314af6
Chaka15205 <minecraftandmore86@gmail.com>
2015-02-12 14:42:05 -0800

    Episode 25 - Math Class Methods

--- src/main/java/com/chaka15205/learn/Main.java
+++ src/main/java/com/chaka15205/learn/Main.java
@@ -2,12 +2,16 @@ package com.chaka15205.learn;
 
 public class Main {
     public static void main(String args[]) {
-        int c = 15;
+        System.out.println(Math.sqrt(9));
 
-        // Do is for at least one run / While is only when true
-        do {
-            System.out.println(c);
-            c++;
-        } while (c <= 10);
+        /**
+         * Math.abs / How Far From 0
+         * Math.ciel / It will round all numbers up i.e 7.4 = 8.0
+         * Math.floor / ciel but rounding down
+         * Math.max / Maximum of 2 numbers (The Bigger Number is true)
+         * Math.min / max but the lower number
+         * Math.power / 10^3 cubed squared 10x10x10
+         * Math.sqrt / Square root (9) = 3
+         */
     }
 }
