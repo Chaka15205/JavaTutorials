1bc419992a11dec7b76389ae70ce215764751beb
Chaka15205 <minecraftandmore86@gmail.com>
2015-02-12 14:57:13 -0800

    Episode 26 - Random Number Generator

--- src/main/java/com/chaka15205/learn/Main.java
+++ src/main/java/com/chaka15205/learn/Main.java
@@ -1,17 +1,15 @@
 package com.chaka15205.learn;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String args[]) {
-        System.out.println(Math.sqrt(9));
+        Random r = new Random();
+        int num;
 
-        /**
-         * Math.abs / How Far From 0
-         * Math.ciel / It will round all numbers up i.e 7.4 = 8.0
-         * Math.floor / ciel but rounding down
-         * Math.max / Maximum of 2 numbers (The Bigger Number is true)
-         * Math.min / max but the lower number
-         * Math.power / 10^3 cubed squared 10x10x10
-         * Math.sqrt / Square root (9) = 3
-         */
+        for (int c=1; c<10; c++) {
+            num = 1+r.nextInt(6); //1+ adds 1 to make it look like 1-6 instead of 0-5
+            System.out.println(num + "\t");
+        }
     }
 }
