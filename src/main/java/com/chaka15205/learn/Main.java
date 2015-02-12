24034f1953eb85ee173879ebc09d7b4ce0125298
Chaka15205 <minecraftandmore86@gmail.com>
2015-02-12 12:50:21 -0800

    Episode 14 - Using Multiple Classes

--- src/main/java/com/chaka15205/learn/Main.java
+++ src/main/java/com/chaka15205/learn/Main.java
@@ -1,14 +1,8 @@
 package com.chaka15205.learn;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String args[]) {
-        int c = 0;
-
-        while (c < 10) {
-            System.out.println(c);
-            c++;
-        }
+        Tuna tuna = new Tuna();
+        tuna.simpleMessage();
     }
 }
---
+++ src/main/java/com/chaka15205/learn/Tuna.java
@@ -0,0 +1,7 @@
+package com.chaka15205.learn;
+
+public class Tuna {
+    public void simpleMessage() {
+        System.out.println("This is from another class.");
+    }
+}
