622b7928165dc53e1c7f57d47ac0ae71d0322772
Chaka15205 <minecraftandmore86@gmail.com>
2015-02-13 18:33:22 -0500

    Episode 28 - Creating An Array Table

--- src/main/java/com/chaka15205/learn/Main.java
+++ src/main/java/com/chaka15205/learn/Main.java
@@ -2,17 +2,11 @@ package com.chaka15205.learn;
 
 public class Main {
     public static void main(String args[]) {
+        System.out.println("Index\tValue");
+        int chaka[]={1,2,3,4,5,6};
 
-        int id[]={1, 2, 3, 4, 5, 6, 7, 8};
-        String staff[]={"Chaka", "Legeis", "Pro_JellyFish", "DJPepper468",
-                "frankie2222222", "stspy212", "jakethe1st", "dragonwing122"};
-
-        int num1 = 1;
-        int num2 = 2;
-
-        int test[]={num1, num2};
-
-
-        System.out.println(test[1]);
+        for (int c=0;c<chaka.length;c++) {
+            System.out.println(c + "\t\t" + chaka[c]);
+        }
     }
 }
