c88b3b7128045233d01acb6d0e54e0a43c5af214
Chaka15205 <minecraftandmore86@gmail.com>
2015-02-13 18:47:29 -0500

    Episode 30 - Array Elements as Counters

--- src/main/java/com/chaka15205/learn/Main.java
+++ src/main/java/com/chaka15205/learn/Main.java
@@ -1,13 +1,20 @@
 package com.chaka15205.learn;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String args[]) {
-        int chaka[]={4,4,4,4};
-        int sum=0;
+        Random rand = new Random();
+        int freq[]= new int[7];
 
-        for (int c=0;c<chaka.length;c++) {
-            sum+=chaka[c];
+        for (int r=1;r<1000;r++) {
+            ++freq[1+rand.nextInt(6)];
         }
-        System.out.println(sum);
+
+        System.out.println("Face\tFrequency");
+
+        for (int f = 1;f<freq.length;f++) {
+            System.out.println(f + "\t\t" + freq[f]);
+        }
     }
 }
