e2d7fc7e455f29458691564b6560c3b3bc6d6473
Chaka15205 <minecraftandmore86@gmail.com>
2015-02-13 18:57:00 -0500

    Episode 31 - Enhanced for Loop

--- src/main/java/com/chaka15205/learn/Main.java
+++ src/main/java/com/chaka15205/learn/Main.java
@@ -1,20 +1,14 @@
 package com.chaka15205.learn;
 
-import java.util.Random;
-
 public class Main {
     public static void main(String args[]) {
-        Random rand = new Random();
-        int freq[]= new int[7];
+        int chaka[]={3,4,5,6,7};
+        int total=0;
 
-        for (int r=1;r<1000;r++) {
-            ++freq[1+rand.nextInt(6)];
+        for (int x: chaka) {
+            total += x;
         }
 
-        System.out.println("Face\tFrequency");
-
-        for (int f = 1;f<freq.length;f++) {
-            System.out.println(f + "\t\t" + freq[f]);
-        }
+        System.out.println(total);
     }
 }
