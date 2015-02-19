2b252ed7ebb9d60469b080b6a4d016979557d01c
Chaka15205 <minecraftandmore86@gmail.com>
2015-02-19 08:31:39 -0800

    Episode 33 - Multidimensional Array

--- src/main/java/com/chaka15205/learn/Main.java
+++ src/main/java/com/chaka15205/learn/Main.java
@@ -2,17 +2,8 @@ package com.chaka15205.learn;
 
 public class Main {
     public static void main(String args[]) {
-        int chaka[]={3,4,5,6,7};
-        updateArray(chaka);
+        int ar1[][]={{8,9,10,11},{12,13,14,15},{16,17,18,19}};
+        int ar2[][]={{1,2,3,4},{42},{6,3,2},{100,200,300,400}};
 
-        for (int y:chaka) {
-            System.out.println(y);
-        }
-    }
-
-    public static void updateArray(int x[]) {
-        for (int c = 0; c<x.length;c++) {
-            x[c]+=5;
-        }
     }
 }
