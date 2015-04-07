520c39cd3c6206a5be1d6938b341fcc7f35511cc
Chaka15205 <minecraftandmore86@gmail.com>
2015-04-07 10:47:49 -0400

    Episode 52 - Event Handling

--- src/main/java/com/chaka15205/learn/Main.java
+++ src/main/java/com/chaka15205/learn/Main.java
@@ -1,13 +1,6 @@
 package com.chaka15205.learn;
 
-import javax.swing.*;
-import java.awt.*;
-
 public class Main {
     public static void main(String[] args) {
-        Util util = new Util();
-        util.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        util.setSize(800, 400);
-        util.setVisible(true);
     }
 }
--- src/main/java/com/chaka15205/learn/Util.java
+++ src/main/java/com/chaka15205/learn/Util.java
@@ -4,14 +4,24 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Util extends JFrame {
-    private JLabel item1;
+
+    private JTextField item1;
+    private JTextField item2;
+    private JTextField item3;
+    private JPasswordField passwordField;
 
     public Util() {
-        super("The Great GUI of Greatness");
+        super("User Login");
         setLayout(new FlowLayout());
 
-        item1 = new JLabel("I am a label");
-        item1.setToolTipText("Hi? I am text?");
+        item1 = new JTextField(10);
         add(item1);
+        item2 = new JTextField("Enter Text Here");
+        add(item2);
+        item3 = new JTextField("Uneditable", 20);
+        item3.setEditable(false);
+        add(item3);
+        passwordField = new JPasswordField("mypassword");
+        add(passwordField);
     }
 }
