fcf70b5bc1e2d13e5bd69a4d163b1360b68458f3
Chaka15205 <minecraftandmore86@gmail.com>
2015-04-06 21:34:37 -0400

    Episode 40 - Set and Get Methods

--- src/main/java/com/chaka15205/learn/Tuna.java
+++ src/main/java/com/chaka15205/learn/Tuna.java
@@ -16,7 +16,7 @@ public class Tuna {
         this(h,m,0);
     }
     public Tuna(int h, int m, int s) {
-        setTime(h,m,s);
+        setTime(h, m, s);
     }
     public void setTime(int h, int m, int s) {
         setHour(h);
@@ -24,6 +24,24 @@ public class Tuna {
         setSecond(s);
     }
     public void setHour(int h) {
-
+        hour = ((h >= 0 && h < 24)? h : 0);
+    }
+    public void setMinute(int m) {
+        minute = ((m >= 0 && m < 60)? m : 0);
+    }
+    public void setSecond(int s) {
+        second = ((s >= 0 && s < 60)? s : 0);
+    }
+    public int getHour() {
+        return hour;
+    }
+    public int getMinute() {
+        return minute;
+    }
+    public int getSecond() {
+        return second;
+    }
+    public String toMilitary() {
+        return String.format("%02d:%02d:02d", getHour(), getMinute(), getSecond());
     }
 }
