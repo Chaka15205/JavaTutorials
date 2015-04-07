42c915f549e8711350a2ca2235984801204d77d4
Chaka15205 <minecraftandmore86@gmail.com>
2015-04-06 22:00:35 -0400

    Episode 43 - Composition

--- src/main/java/com/chaka15205/learn/Main.java
+++ src/main/java/com/chaka15205/learn/Main.java
@@ -3,5 +3,8 @@ package com.chaka15205.learn;
 public class Main {
     public static void main(String [] args) {
         Ref ref = new Ref(4, 5, 6);
+        Util util = new Util("Chris", ref);
+
+        System.out.println(util);
     }
 }
