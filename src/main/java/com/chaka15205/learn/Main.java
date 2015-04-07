be47feab7205e60a3d1a942df8713df41275db04
Chaka15205 <minecraftandmore86@gmail.com>
2015-04-06 21:26:13 -0400

    Episode 39 - Multiple Constructors

--- src/main/java/com/chaka15205/learn/Main.java
+++ src/main/java/com/chaka15205/learn/Main.java
@@ -2,11 +2,6 @@ package com.chaka15205.learn;
 
 public class Main {
     public static void main(String [] args) {
-        Tuna tuna = new Tuna();
-        System.out.println(tuna.toMilitary());
-        System.out.println(tuna.toString());
 
-        System.out.println(tuna.toMilitary());
-        System.out.println(tuna.toString());
     }
 }
--- src/main/java/com/chaka15205/learn/Tuna.java
+++ src/main/java/com/chaka15205/learn/Tuna.java
@@ -1,20 +1,29 @@
 package com.chaka15205.learn;
 
 public class Tuna {
-    private int hour = 1;
-    private int minute = 2;
-    private int second = 3;
 
-    public void setTime(int hour, int minute, int second) {
-        this.hour = 4;//((hour >= 0 && hour < 24) ? hour : 0);
-        this.minute = 5;//((minute >= 0 && minute < 60) ? minute : 0);
-        this.second = 6;//((second >= 0 && second < 60) ? second : 0);
-    }
+    private int hour;
+    private int minute;
+    private int second;
 
-    public String toMilitary() {
-        return String.format("%02d:%02d:%02d", hour, minute, second);
+    public Tuna() {
+        this(0,0,0);
     }
-    public String toString() {
-        return String.format("%d:%02d:%02d %s", ((hour==0||hour==12)?12:hour%12), minute, second, (hour < 12?"AM":"PM"));
+    public Tuna(int h) {
+        this(h,0,0);
+    }
+    public Tuna(int h, int m) {
+        this(h,m,0);
+    }
+    public Tuna(int h, int m, int s) {
+        setTime(h,m,s);
+    }
+    public void setTime(int h, int m, int s) {
+        setHour(h);
+        setMinute(m);
+        setSecond(s);
+    }
+    public void setHour(int h) {
+
     }
 }
