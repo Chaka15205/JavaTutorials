c2b049b3dcf8354f9abb2e8566140649a8b63c66
Chaka15205 <minecraftandmore86@gmail.com>
2015-04-07 11:17:31 -0400

    Episode 53 - ActionListener

--- src/main/java/com/chaka15205/learn/Util.java
+++ src/main/java/com/chaka15205/learn/Util.java
@@ -2,6 +2,8 @@ package com.chaka15205.learn;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Util extends JFrame {
 
@@ -23,5 +25,26 @@ public class Util extends JFrame {
         add(item3);
         passwordField = new JPasswordField("mypassword");
         add(passwordField);
+
+        TheHandler handler = new TheHandler();
+        item1.addActionListener(handler);
+        item2.addActionListener(handler);
+        item3.addActionListener(handler);
+        passwordField.addActionListener(handler);
+    }
+    private class TheHandler implements ActionListener {
+
+        public void actionPerformed(ActionEvent event) {
+            String string = "";
+            if (event.getSource() == item1) {
+                string = String.format("field 1: %s", event.getActionCommand());
+            } else if (event.getSource() == item2) {
+                string = String.format("field 2: %s", event.getActionCommand());
+            } else if (event.getSource() == item3) {
+                string = String.format("field 3: %s", event.getActionCommand());
+            } else if (event.getSource() == passwordField) {
+                string = String.format("password field is: %s", event.getActionCommand());
+            }
+        }
     }
 }
